5cf7beec5ac9886336d2797c888b2c49ac91b60f
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-05-19 03:35:29 +0600

    context api

--- src/Components/Grandpa/Grandpa.js
+++ src/Components/Grandpa/Grandpa.js
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import './Grandpa.css'
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 
 const Grandpa = () => {
+    // Context api text
+    const RingContext = createContext('Dimon Ding')
+    //
     const [house, setHouse] = useState(0);
-
     const ornament = 'Dimon Ring'
-
     const handleBuyAHouse = () => {
         const newHouseCount = house + 1;
         setHouse(newHouseCount);
     }
     return (
-        <div className='grandpa' >
-            <h3>Grandpa</h3>
-            <p>House: {house}<button onClick={() => handleBuyAHouse()}>Buy a house</button></p>
-            <div className="family">
-                <Father house={house} ornament={ornament}></Father>
-                <Uncle house={house}></Uncle>
-                <Aunty house={house}></Aunty>
+        <RingContext.Provider>
+            <div className='grandpa' >
+                <h3>Grandpa</h3>
+                <p>House: {house}<button onClick={() => handleBuyAHouse()}>Buy a house</button></p>
+                <div className="family">
+                    <Father house={house} ornament={ornament}></Father>
+                    <Uncle house={house}></Uncle>
+                    <Aunty house={house}></Aunty>
+                </div>
             </div>
-        </div>
+        </RingContext.Provider>
+
     );
 };
 
